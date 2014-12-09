72ec1ec8ebfdbdc6642051580f9e4ad046762d8c
Marjan Bachtiari <marjan.bachtiari@haw-hamburg.de>
2014-12-09 19:47:29 +0100

    Changed pickRandom to pickRandomIndex since we need indices for the swap

--- src/myUtil.erl
+++ src/myUtil.erl
@@ -10,27 +10,27 @@
 -author("KamikazeOnRoad").
 
 %% API
--export([pickRandom/1, pickRandom/3, getIndex/2]).
+-export([pickRandomIndex/1, pickRandomIndex/3, getIndex/2]).
 -import(arrayS, [lengthA/1, getA/2]).
 
 %% Returns random elem in array
 %% Returns nil if array is empty
-pickRandom({}) -> nil;
-pickRandom(Array) ->
+pickRandomIndex({}) -> nil;
+pickRandomIndex(Array) ->
   % random:uniform(N) generates random Integer between 1 and N
   % To get index 0, too, need to subtract 1 at the end, therefore length array as N
-  RandomIndex = random:uniform(lengthA(Array)) - 1,
-  RandomElem = getA(Array, RandomIndex),
-  RandomElem.
+  random:uniform(lengthA(Array)) - 1.
 
 %% Returns random element from specified range in array
 %% Returns nil if array is empty or Left is bigger than Right
-pickRandom({}, _, _) -> nil;
-pickRandom(_, Left, Right) when Left > Right -> nil;
-pickRandom(Array, Left, Right) when Left < Right ->
-  RandomIndex = random:uniform(Right-Left+1) + (Left-1),
-  RandomElem = getA(Array, RandomIndex),
-  RandomElem.
+pickRandomIndex({}, _, _) -> nil;
+pickRandomIndex(_, Left, Right) when Left > Right -> nil;
+pickRandomIndex(Array, Left, Right) when Left < Right ->
+  Length = lengthA(Array),
+  if
+    Right < Length -> random:uniform(Right-Left+1) + (Left-1);
+    Right >= Length -> random:uniform(Length-Left) + (Left-1)
+  end.
 
 %% Returns index of elem in array,
 %% returns false if elem is not in array
